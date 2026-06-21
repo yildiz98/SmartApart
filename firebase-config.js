ab048aed6dd903720314462e9e988dbd7bdbecfe
yildiz98 <yildiz.21.98@gmail.com>
2026-06-21 14:36:52 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,29 +1,19 @@
-// EMFE 3 SmartApart Firebase Ayarları
-// 1) Firebase Console > Project settings > Web app config bilgilerini aşağıya yapıştır.
-// 2) enabled değerini true yap.
-// 3) Firestore Database'i oluştur.
-// 4) Authentication > Sign-in method > Anonymous veya Email/Password aç.
-//
-// Not: adminEmail/adminPassword boş kalırsa anonim giriş kullanılır.
-
 window.EMFE3_FIREBASE = {
-  enabled: false,
+  enabled: true,
 
   config: {
-    apiKey: "BURAYA_API_KEY",
-    authDomain: "BURAYA_PROJECT.firebaseapp.com",
-    projectId: "BURAYA_PROJECT_ID",
-    storageBucket: "BURAYA_PROJECT.appspot.com",
-    messagingSenderId: "BURAYA_SENDER_ID",
-    appId: "BURAYA_APP_ID"
+    apiKey: "AIzaSyCPB0AY7cRjJ-DhS0YFPF4Y0Y2ZJUsXIN0",
+    authDomain: "smartapart-ab8de.firebaseapp.com",
+    projectId: "smartapart-ab8de",
+    storageBucket: "smartapart-ab8de.firebasestorage.app",
+    messagingSenderId: "875587120223",
+    appId: "1:875587120223:web:b60b64df851fd40052ffea",
+    measurementId: "G-DKR5RHYYBX"
   },
 
-  // Firestore konumu: smartapart / emfe3
   collection: "smartapart",
   docId: "emfe3",
 
-  // İstersen yönetici e-posta/şifre girişi kullan.
-  // Boş bırakırsan anonim giriş dener.
-  adminEmail: "",
-  adminPassword: ""
+  adminEmail: "yonetim@emfe3.com",
+  adminPassword: "Poh1234"
 };
